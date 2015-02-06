b9d7db3478066bb33900d3f8f191311cb60f9ec6
Lokstar <patrykgruzlewski@gmail.com>
2015-02-06 19:33:20 +0100

    add project to git

---
+++ node/public/js/app.js
@@ -0,0 +1,27 @@
+
+var http = require('http');
+var fs = require('fs');
+var count = 0;
+
+var server = http.createServer(function(req,res){
+    fs.readFile('./index.html', function(error, data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(data, 'utf-8');
+    });
+}).listen(3000);
+
+console.log('Server is running');
+
+var io = require('socket.io').listen(server);
+
+
+
+io.sockets.on('connection', function(socket){
+    console.log('User connected');
+    setInterval(function(){
+    fs.readFile('/sys/bus/w1/devices/28-000006c01dc8/w1_slave', function(error, data){
+        console.log(data.toString().substring(69)/1000);
+        socket.emit('temperature',{number: data.toString().substring(69)/1000});
+    });
+    }, 1000);
+});
